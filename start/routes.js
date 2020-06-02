d89f6438a9b53d52073e59d0c40095488b9e456c
Vinicius Okamoto <leo_kamoto@hotmail.com>
2020-06-02 07:10:39 -0300

    forgot password done

--- start/routes.js
+++ start/routes.js
@@ -1,8 +1,11 @@
 'use strict'
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
 Route.post('users', 'UserController.store')
+Route.post('sessions', 'SessionController.store')
+
+Route.post('passwords', 'ForgotPasswordController.store')
+Route.put('passwords', 'ForgotPasswordController.update')
 
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
